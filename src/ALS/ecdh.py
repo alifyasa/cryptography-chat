f1141a40d41306828b58ee515c5fad736aa5f838
gilangr301102 <muhgilangramadhan.3011@gmail.com>
2024-05-26 04:26:14 +0700

    fx ecdh return

--- src/ALS/ecdh.py
+++ src/ALS/ecdh.py
@@ -13,22 +13,15 @@ def generate_shared_secret(private_key, public_key, a, p):
     shared_secret = point_multiplication(private_key, public_key, a, p)
     return shared_secret[0]  # Return only the x-coordinate as shared secret
 
-if __name__ == "__main__":
-    # Initialize curve parameters
+def getAllKeyECDH():
     randomizerCurve = RandomizerCurve()
     a, b, gx, gy, p = randomizerCurve.get_all_parameters()
     G = (gx, gy)
     n = 19  # Order of the generator point (this should typically be the order of the base point, needs to be adapted for your curve)
-
-    # Alice's side
     alice_private_key, alice_public_key = generate_key(n, G, a, p)
-    print("Alice's private key:", alice_private_key)
-    print("Alice's public key:", alice_public_key)
 
     # Bob's side
     bob_private_key, bob_public_key = generate_key(n, G, a, p)
-    print("Bob's private key:", bob_private_key)
-    print("Bob's public key:", bob_public_key)
 
     # Alice computes shared secret
     alice_shared_secret = generate_shared_secret(alice_private_key, bob_public_key, a, p)
@@ -39,4 +32,17 @@ if __name__ == "__main__":
     # Both should have the same shared secret
     assert alice_shared_secret == bob_shared_secret
 
-    print("Shared Secret:", alice_shared_secret)
+    return bob_private_key, bob_public_key, alice_private_key, alice_public_key, alice_shared_secret
+
+if __name__ == "__main__":
+    # Initialize curve parameters
+    bobPrivKey, bobPubKey, alicePrivKey, alicePubKey, sharedKey = getAllKeyECDH()
+
+    print("Bob's private key:", bobPrivKey)
+    print("Bob's public key:", bobPubKey)
+
+    print("Alice's private key:", alicePrivKey)
+    print("Alice's public key:", alicePubKey)
+
+    print("Shared secret:", sharedKey)
+
